a6c6bacc62c83fd06d2215583378b3a4ea7a08b5
m87y92vvjd-dotcom <m87y92vvjd@privaterelay.appleid.com>
2026-09-21 23:11:03 -0400

    Repair corrupted JUCE build and source files so the upgrade branch can configure and compile.

--- Source/DeckComponent.cpp
+++ Source/DeckComponent.cpp
@@ -1,33 +1,33 @@
-#pragma once
-
-#include <functional>
-#include <juce_gui_extra/juce_gui_extra.h>
-
-class DeckComponent : public juce::Component
+#include "DeckComponent.h"
+#include <utility>
+DeckComponent::DeckComponent(const juce::String& name)
 {
-public:
-    explicit DeckComponent(const juce::String& deckName);
-    ~DeckComponent() override = default;
-
-    void paint(juce::Graphics&) override;
-    void resized() override;
-    void setLoadCallback(std::function<void()> callback);
-    void setPlayCallback(std::function<void()> callback);
-    void setStopCallback(std::function<void()> callback);
-    void setGainCallback(std::function<void(float)> callback);
-    void setSeekCallback(std::function<void(double)> callback);
-    void setStatusText(const juce::String& text);
-    void setTrackText(const juce::String& text);
-    void setPlayback(double position, double length, float level);
-
-private:
-    juce::Label nameLabel, statusLabel, trackLabel, timeLabel;
-    juce::Slider volumeSlider, bpmSlider, positionSlider;
-    juce::TextButton loadButton, playButton, stopButton;
-    std::function<void()> onLoad = [] {};
-    std::function<void()> onPlay = [] {};
-    std::function<void()> onStop = [] {};
-    std::function<void(float)> onGain = [](float) {};
-    std::function<void(double)> onSeek = [](double) {};
-    float outputLevel = 0.0f;
-};
+    nameLabel.setText(name, juce::dontSendNotification); nameLabel.setFont(juce::Font(24.0f, juce::Font::bold)); addAndMakeVisible(nameLabel);
+    statusLabel.setText("Ready to spin", juce::dontSendNotification); addAndMakeVisible(statusLabel);
+    trackLabel.setText("No track loaded", juce::dontSendNotification); addAndMakeVisible(trackLabel);
+    timeLabel.setText("00:00 / 00:00", juce::dontSendNotification); timeLabel.setJustificationType(juce::Justification::centredRight); addAndMakeVisible(timeLabel);
+    volumeSlider.setRange(0.0, 100.0, 0.1); volumeSlider.setValue(68.0); volumeSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 80, 20); volumeSlider.onValueChange = [this] { onGain(static_cast<float>(volumeSlider.getValue() / 100.0)); }; addAndMakeVisible(volumeSlider);
+    bpmSlider.setRange(70.0, 180.0, 0.1); bpmSlider.setValue(128.0); bpmSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 80, 20); addAndMakeVisible(bpmSlider);
+    positionSlider.setRange(0.0, 1.0, 0.001); positionSlider.setTextBoxStyle(juce::Slider::NoTextBox, false, 0, 0); positionSlider.onValueChange = [this] { if (positionSlider.isMouseButtonDown()) onSeek(positionSlider.getValue()); }; addAndMakeVisible(positionSlider);
+    loadButton.setButtonText("Load"); loadButton.onClick = [this] { onLoad(); }; addAndMakeVisible(loadButton);
+    playButton.setButtonText("Play"); playButton.onClick = [this] { onPlay(); }; addAndMakeVisible(playButton);
+    stopButton.setButtonText("Stop"); stopButton.onClick = [this] { onStop(); }; addAndMakeVisible(stopButton);
+}
+void DeckComponent::setLoadCallback(std::function<void()> c) { onLoad = std::move(c); } void DeckComponent::setPlayCallback(std::function<void()> c) { onPlay = std::move(c); } void DeckComponent::setStopCallback(std::function<void()> c) { onStop = std::move(c); } void DeckComponent::setGainCallback(std::function<void(float)> c) { onGain = std::move(c); } void DeckComponent::setSeekCallback(std::function<void(double)> c) { onSeek = std::move(c); }
+void DeckComponent::setStatusText(const juce::String& t) { statusLabel.setText(t, juce::dontSendNotification); } void DeckComponent::setTrackText(const juce::String& t) { trackLabel.setText(t, juce::dontSendNotification); }
+void DeckComponent::setPlayback(double position, double length, float level)
+{
+    const auto safe = juce::jmax(0.0, length); const auto progress = safe > 0.0 ? juce::jlimit(0.0, 1.0, position / safe) : 0.0;
+    positionSlider.setValue(progress, juce::dontSendNotification);
+    timeLabel.setText(juce::String::formatted("%02d:%02d / %02d:%02d", static_cast<int>(position) / 60, static_cast<int>(position) % 60, static_cast<int>(safe) / 60, static_cast<int>(safe) % 60), juce::dontSendNotification);
+    outputLevel = juce::jlimit(0.0f, 1.0f, level); repaint();
+}
+void DeckComponent::paint(juce::Graphics& g)
+{
+    const auto area = getLocalBounds().toFloat(); g.setColour(juce::Colour(0xff1c212d)); g.fillRoundedRectangle(area.reduced(6.0f), 20.0f); g.setColour(juce::Colour(0xff2d3745)); g.fillRoundedRectangle(area.reduced(16.0f), 18.0f);
+    const auto meter = area.withTrimmedLeft(18.0f).withTrimmedRight(18.0f).withY(92.0f).withHeight(8.0f); g.setColour(juce::Colours::black.withAlpha(0.35f)); g.fillRoundedRectangle(meter, 4.0f); g.setColour(juce::Colour(0xff58d68d)); g.fillRoundedRectangle(meter.withWidth(meter.getWidth() * outputLevel), 4.0f); g.setColour(juce::Colours::white.withAlpha(0.16f)); g.drawRoundedRectangle(area.reduced(12.0f), 18.0f, 1.0f);
+}
+void DeckComponent::resized()
+{
+    auto area = getLocalBounds().reduced(18); nameLabel.setBounds(area.removeFromTop(34)); statusLabel.setBounds(area.removeFromTop(20)); trackLabel.setBounds(area.removeFromTop(24)); timeLabel.setBounds(area.removeFromTop(20)); positionSlider.setBounds(area.removeFromTop(30).reduced(4, 8)); auto controls = area.removeFromTop(110); volumeSlider.setBounds(controls.removeFromLeft(controls.getWidth() / 2).reduced(4)); bpmSlider.setBounds(controls.reduced(4)); auto buttons = area.removeFromTop(44); loadButton.setBounds(buttons.removeFromLeft(90).reduced(4)); playButton.setBounds(buttons.removeFromLeft(90).reduced(4)); stopButton.setBounds(buttons.removeFromLeft(90).reduced(4));
+}
--- Source/MixerComponent.cpp
+++ Source/MixerComponent.cpp
@@ -1,28 +1,14 @@
-#pragma once
-
-#include <functional>
-#include <juce_gui_extra/juce_gui_extra.h>
-
-class MixerComponent : public juce::Component
+#include "MixerComponent.h"
+#include <utility>
+MixerComponent::MixerComponent()
 {
-public:
-    MixerComponent();
-    ~MixerComponent() override = default;
-
-    void paint(juce::Graphics&) override;
-    void resized() override;
-
-    void setCrossfadeCallback(std::function<void(float)> callback);
-    void setMasterGainCallback(std::function<void(float)> callback);
-
-private:
-    juce::Label titleLabel;
-    juce::Label crossfadeLabel;
-    juce::Label masterLabel;
-    juce::Slider crossfader;
-    juce::Slider masterGain;
-    juce::Slider channelGain;
-
-    std::function<void(float)> onCrossfade = [](float) {};
-    std::function<void(float)> onMasterGain = [](float) {};
-};
+    titleLabel.setText("MIXER", juce::dontSendNotification); titleLabel.setFont(juce::Font(16.0f, juce::Font::bold)); titleLabel.setJustificationType(juce::Justification::centred); addAndMakeVisible(titleLabel);
+    crossfadeLabel.setText("Crossfader", juce::dontSendNotification); crossfadeLabel.setJustificationType(juce::Justification::centred); addAndMakeVisible(crossfadeLabel);
+    crossfader.setRange(-1.0, 1.0, 0.01); crossfader.setValue(0.0); crossfader.setTextBoxStyle(juce::Slider::NoTextBox, false, 0, 0); crossfader.onValueChange = [this] { onCrossfade(static_cast<float>(crossfader.getValue())); }; addAndMakeVisible(crossfader);
+    masterLabel.setText("Master", juce::dontSendNotification); masterLabel.setJustificationType(juce::Justification::centred); addAndMakeVisible(masterLabel);
+    masterGain.setRange(0.0, 1.0, 0.01); masterGain.setValue(0.85); masterGain.setSliderStyle(juce::Slider::LinearVertical); masterGain.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 70, 20); masterGain.onValueChange = [this] { onMasterGain(static_cast<float>(masterGain.getValue())); }; addAndMakeVisible(masterGain);
+    channelGain.setRange(0.0, 1.0, 0.01); channelGain.setValue(0.75); channelGain.setSliderStyle(juce::Slider::LinearVertical); channelGain.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 70, 20); addAndMakeVisible(channelGain);
+}
+void MixerComponent::setCrossfadeCallback(std::function<void(float)> c) { onCrossfade = std::move(c); } void MixerComponent::setMasterGainCallback(std::function<void(float)> c) { onMasterGain = std::move(c); }
+void MixerComponent::paint(juce::Graphics& g) { auto a = getLocalBounds().toFloat(); g.setColour(juce::Colour(0xff151c2b)); g.fillRoundedRectangle(a.reduced(5.0f), 18.0f); g.setColour(juce::Colours::white.withAlpha(0.15f)); g.drawRoundedRectangle(a.reduced(5.0f), 18.0f, 1.0f); }
+void MixerComponent::resized() { auto a = getLocalBounds().reduced(14); titleLabel.setBounds(a.removeFromTop(28)); crossfadeLabel.setBounds(a.removeFromTop(22)); crossfader.setBounds(a.removeFromTop(42).reduced(4, 8)); masterLabel.setBounds(a.removeFromTop(22)); auto f = a; masterGain.setBounds(f.removeFromLeft(f.getWidth() / 2).reduced(10, 4)); channelGain.setBounds(f.reduced(10, 4)); }
